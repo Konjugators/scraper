b33f817d577e258783c9abb9473ebffd26e59506
shynn <shynn.lawrence@gmail.com>
2020-11-11 13:12:19 -0800

    Aditya: Scraper Shynn: CSVinterface Co-authored-by: Aditya Matam <aditya0310@outlook.com>

---
+++ csvinterface.py
@@ -0,0 +1,32 @@
+def verbreturns():
+    return infinitives
+
+def formatting(verbs):
+    length = len(verbs)
+    bad_things = ["⁵", "³", "²"]
+    for i in range(length):
+        for z in bad_things:
+            verbs[i] = verbs[i].replace(z, "")
+        verbs[i] = verbs[i].strip()        
+    return verbs
+
+
+def appverbs(z:list):
+    with open("new_verbs.csv", "a") as newverbs:
+        csvWriter = csv.writer(newverbs, delimiter=",")
+        csvWriter.writerows(z)
+    print("finished")
+
+if __name__ == "__main__" or __name__ != "__main__":
+    import csv
+    global infinitives
+    global conjugations
+    conjugations = []
+    infinitives = []
+    import os
+    this_dir, this_filename = os.path.split(__file__)
+    path = os.path.join(this_dir, "verbs.csv")
+    with open(path, "r", newline="") as file:
+        verblist = csv.reader(file)
+        for row in verblist:
+            infinitives.append(row[0])
--- main.py
+++ main.py
@@ -1,5 +1,62 @@
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
+# "www.verbformen.de"
+# verbs = ["machen"]
+
+import csvinterface as csvi
+import re
+import requests
+from bs4 import BeautifulSoup as bs
+
+# PARSES ONE CONJUGATION OF A CERTAIN TENSE
+def trtext(tr)->str:
+    outstr = []
+    for td in tr.find_all("td"):
+        for item in td.contents:
+            notags = re.sub("<.*?>", "", str(item))
+            outstr.append(notags)
+    return "".join(outstr)
+
+def get_conjugations(verb: str)->list:
+    # sleep(1)
+    url = f"https://www.verbformen.de/konjugation/?w={verb}"
+    r = requests.get(url)
+    soup = bs(r.text, "html5lib")
+
+    mainsection = ""
+
+    maindiv = soup.find_all("div", attrs={"class": "rAbschnitt"})
+    for section in maindiv[0].find_all("section"):
+        verbtype = section.find("header").find("h2")
+        if verbtype is not None and verbtype.text == "Indikativ":
+            mainsection = section
+            break
+    else:
+        return []
+
+    # ITER THRU EACH TENSE
+    conjugs = []
+    for table in mainsection.find_all("div", class_="vTbl"):
+        for tr in table.find_all("tr"):
+            conjugs.append(trtext(tr))
+    
+    return conjugs
+
+infinitives = csvi.verbreturns()
+bigverbs = []
+zed = 0
+from time import sleep
+for verb in infinitives:
+    try:
+        oneVerbConjugs = get_conjugations(verb)
+        if len(oneVerbConjugs) == 0:
+            continue
+        oneVerbConjugs.insert(0, verb)
+        # print(oneVerbConjugs)
+        if zed == 9:
+            break
+        zed += 1
+        csvi.appverbs([csvi.formatting(oneVerbConjugs)])
+        # bigverbs.append(oneVerbConjugs)
+    except:
+        print(f"Err on {verb}")
+# "https://www.verbformen.de/konjugation/?w=machen"
+# csvi.appverbs(bigverbs)
