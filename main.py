ad5deef4563ba94fb348732b4046ee5887d16461
Aditya Matam <aditya0310@outlook.com>
2020-11-11 15:03:33 -0800

    scraping with selenium

--- main.py
+++ main.py
@@ -1,25 +1,44 @@
 # "www.verbformen.de"
 # verbs = ["machen"]
 
-import csvinterface as csvi
 import re
+from random import randint
+from time import sleep
+
 import requests
 from bs4 import BeautifulSoup as bs
+from selenium import webdriver
+
+import csvinterface as csvi
+
+gecko = r"C:\Users\adity\Downloads\geckodriver-v0.27.0-win64\geckodriver.exe"
+driver = webdriver.Firefox(executable_path=gecko)
 
 # PARSES ONE CONJUGATION OF A CERTAIN TENSE
-def trtext(tr)->str:
+def trtext(tr) -> str:
     outstr = []
+    badchars = {"¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹", "⁰"}
     for td in tr.find_all("td"):
         for item in td.contents:
             notags = re.sub("<.*?>", "", str(item))
+            for exp in badchars:
+                notags = notags.replace(exp, "")
             outstr.append(notags)
     return "".join(outstr)
 
-def get_conjugations(verb: str)->list:
-    # sleep(1)
+
+def get_conjugations(verb: str) -> list:
     url = f"https://www.verbformen.de/konjugation/?w={verb}"
+    driver.get(url)
+    sleep(2)
+
+    driver.execute_script(
+        "window.scrollTo(0, document.body.scrollHeight);return document.body.scrollHeight;"
+    )
+    sleep(2)
+
     r = requests.get(url)
-    soup = bs(r.text, "html5lib")
+    soup = bs(r.text, "lxml")
 
     mainsection = ""
 
@@ -33,33 +52,31 @@ def get_conjugations(verb: str)->list:
         return []
 
     # ITER THRU EACH TENSE
-    conjugs = []
+    conjugs = [verb]
     for table in mainsection.find_all("div", class_="vTbl"):
         for tr in table.find_all("tr"):
             conjugs.append(trtext(tr))
-    
     return conjugs
 
+
 infinitives = csvi.verbreturns()
-bigverbs = []
+del infinitives[0]
 zed = 0
-from time import sleep
-from random import randint
-for verb in infinitives:
-    try:
-        oneVerbConjugs = get_conjugations(verb)
-        # print(oneVerbConjugs)
-        if len(oneVerbConjugs) == 0:
-            continue
-        oneVerbConjugs.insert(0, verb)
-        # print(oneVerbConjugs)
-        zed += 1
-        csvi.appverbs([oneVerbConjugs])
-        print(f"successfully conjugated {verb}")
-        # bigverbs.append(oneVerbConjugs)
-    except:
-        print(f"Err on {verb}")
-        sleep(randint(2, 6))
-# "https://www.verbformen.de/konjugation/?w=machen"
-# csvi.appverbs(bigverbs)
-print(f"Conjugated {zed} verbs")
+
+try:
+    for verb in infinitives:
+        try:
+            oneVerbConjugs = get_conjugations(verb)
+            if len(oneVerbConjugs) == 0:
+                continue
+            csvi.txtwrite(oneVerbConjugs)
+            print(f"successfully conjugated {verb}")
+            zed += 1
+        except:
+            print(f"error on {verb}")
+            sleep(1)
+except KeyboardInterrupt:
+    pass
+finally:
+    print(f"Conjugated {zed} verbs")
+    driver.quit()
